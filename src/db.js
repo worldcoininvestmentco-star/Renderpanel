a9835cfe6f04d44795e6fbc6d798614739802d3a
worldcoininvestmentco-star <worldcoininvestment.co@gmail.com>
2025-10-01 17:11:57 +0300

    Delete src directory

--- src/db.js
+++
@@ -1,5 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-module.exports = function(){
-  const db = new sqlite3.Database('./data.sqlite');
-  return db;
-};
--- src/routes/billing.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../db')();
-
-router.get('/plans', (req,res)=>{
-  db.all('SELECT * FROM plans', [], (err, rows)=> res.render('plans', { plans: rows || [] }));
-});
-
-module.exports = router;
